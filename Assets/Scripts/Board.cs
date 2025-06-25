9895bfe7d63c44fd50aea2add585eb69ed04f03c
fullstackboss <romeltejada@gmail.com>
2025-06-25 16:44:46 -0500

    3. haciendo la cuadricula
    Generando la cuadricula

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+
+    public int width;
+    public int height;
+    public GameObject tileObject;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SetupBoard();
+    }
+    private void SetupBoard()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                var o = Instantiate(tileObject, new Vector3(x, y, -5), Quaternion.identity);
+                o.transform.parent = transform;
+            }
+         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
